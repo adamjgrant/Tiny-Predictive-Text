ca53159d6e734ba979e2b5682fea0102a6fd1b58
AG <a@b.com>
2024-03-08 10:37:35 -0800

    Adding failing tests for refactor

--- test.py
+++ test.py
@@ -9,53 +9,53 @@ class TestFiling(unittest.TestCase):
         context_words = { "context_window": ["first", "second"], "anchor": "anchor" }
         predictive_words = ["a", "b", "c"]
         actual = finish_filing(tree_store, context_words, predictive_words)
-        expected = {"anchor": {"-": 1, "second": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}}}}
-        
+        expected = { "anchor": { "score": 1, "second": { "score": 1, "first": { "score": 1, "prediction": [ {"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1} ] } } } }
+
         self.assertEqual(actual, expected)
 
 class TestCreateDictionary(unittest.TestCase):
     def test_basic_input(self):
-      tree = {"anchor": {"-": 1, "second": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}}}} 
+      tree = { "anchor": { "score": 1, "second": { "score": 1, "first": { "score": 1, "prediction": [ {"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1} ] } } } }
       pruned_tree = create_dictionary(tree, 1000)
 
       self.assertEqual(pruned_tree, tree)
 
     def test_pruning_input(self):
-      tree = {"anchor": {"-": 2, "second": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}}}, 
-              "anchor2": {"-": 1, "second": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}}}} 
-      expected_pruned_tree = {"anchor": {"-": 2, "second": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}}}} 
+      tree = { "anchor": { "score": 2, "second": { "score": 1, "first": { "score": 1, "prediction": [ {"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1} ] } } }, 
+        "anchor2": { "score": 1, "second": { "score": 1, "first": { "score": 1, "prediction": [ {"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1} ] } } } }
+      expected_pruned_tree = { "anchor": { "score": 2, "second": { "score": 1, "first": { "score": 1, "prediction": [ {"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1} ] } } } }      
       pruned_tree = create_dictionary(tree, 1)
 
       self.assertEqual(pruned_tree, expected_pruned_tree)
 
     def test_doesnt_prune_special_keys(self):
-      tree = {"anchor": 
-                {
-                  "second1": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "-": 2, 
-                  "second2": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "second3": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "second4": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "second5": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "second6": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                }
-             }
-      expected_pruned_tree = {"anchor": 
-                {
-                  "second1": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "-": 2, 
-                  "second2": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "second3": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                  "second4": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}},
-                }
-             }
+      tree = {
+          "anchor": {
+              "second1": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "score": 2,
+              "second2": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "second3": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "second4": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "second5": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "second6": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}}
+          }
+      }
+      expected_pruned_tree = {
+          "anchor": {
+              "second1": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "score": 2,
+              "second2": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "second3": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}},
+              "second4": {"score": 1, "first": {"score": 1, "prediction": [{"prediction": ["a", "a2", "a3"], "score": 1}, {"prediction": ["b", "b2", "b3"], "score": 1}, {"prediction": ["c", "c2", "c3"], "score": 1}]}}
+          }
+      }
       pruned_tree = create_dictionary(tree, 1)
 
       self.assertEqual(pruned_tree, expected_pruned_tree)
 
     def test_token_dict(self):
-      tree = {"anchor": {"-": 1, "second": {"-": 1, "first": {"-": 1, "_": {"a": 1, "b": 1, "c": 1}}}}} 
-      expected_tokenized_tree = {0: {"-": 1, 1: {"-": 1, 2: {"-": 1, "_": {3: 1, 4: 1, 5: 1}}}}} 
+      tree = {"anchor": {"score": 1, "second": {"score": 1, "first": {"score": 1, "prediction": [{ "prediction": ["a", "a2", "a3"], "score": 1}, { "prediction": ["b", "b2", "b3"], "score": 1 }, { "prediction": ["c", "c2", "c3"], "score": 1 } ]}}}} 
+      expected_tokenized_tree = {0: {"score": 1, 1: {"score": 1, 2: {"score": 1, "prediction": [{ "prediction": [3,4,5], "score": 1 }, { "prediction": [6,7,8], "score": 1 }, { "prediction": [9,10,11], "score": 1 }]}}}} 
       expected_dict = {}
       actual_tokenized_tree, actual_dict = create_token_dict(tree)
 
