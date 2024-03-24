9775adbd2b6190fbfcec5d8b6b62a9fcda7c65a9
Your Name <you@example.com>
2024-03-24 08:37:23 -0700

    Adding pkg

---
+++ wasm/pkg/tinypredict_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function load_dictionary(a: number, b: number, c: number): void;
+export function load_tokens(a: number, b: number, c: number): void;
+export function get_predictive_text(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
